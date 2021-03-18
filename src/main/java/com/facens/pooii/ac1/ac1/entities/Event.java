f27ad71d486971ac5f9a226c1ab42c0916b3c248
Gabriela Miranda <gabi.m.domingues@hotmail.com>
2021-03-18 19:16:17 -0300

    Entidade e Controller

---
+++ src/main/java/com/facens/pooii/ac1/ac1/entities/Event.java
@@ -0,0 +1,70 @@
+package com.facens.pooii.ac1.ac1.entities;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Event {
+    private Long id;
+    private String name;
+    private String description;
+    private String place;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String emailContact;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getPlace() {
+        return place;
+    }
+    public void setPlace(String place) {
+        this.place = place;
+    }
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+    public String getEmailContact() {
+        return emailContact;
+    }
+    public void setEmailContact(String emailContact) {
+        this.emailContact = emailContact;
+    }
+}
