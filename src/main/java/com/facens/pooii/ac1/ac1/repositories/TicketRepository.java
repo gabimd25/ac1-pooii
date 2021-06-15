f14ba42c02b1b650427b62cdd2cd1c577487291d
Gabriela Miranda <gabi.m.domingues@hotmail.com>
2021-06-15 20:23:20 -0300

    data.sql arrumado

--- src/main/java/com/facens/pooii/ac1/ac1/repositories/TicketRepository.java
+++ src/main/java/com/facens/pooii/ac1/ac1/repositories/TicketRepository.java
@@ -1,19 +1,13 @@
 package com.facens.pooii.ac1.ac1.repositories;
 
-import java.util.List;
 
 import com.facens.pooii.ac1.ac1.entities.Ticket;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long>{
-
-    /*@Query("SELECT e FROM Ticket e "+ "WHERE " + 
-        "e.event.id = :id" )
-    public List<Ticket> freeTickets(Long id);*/
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,3 @@
-INSERT INTO TB_EVENT (name, description, start_date, end_date, start_time, end_time, email_contact,amount_free_tickets,amount_payed_tickets,price_ticket) VALUES ('curso de culinária','culinária francesa','2021-10-03','2021-10-05','19:30','10:00', 'gabi@gmail.com',10,10,10.0 )
-INSERT INTO TB_EVENT (name, description, start_date, end_date, start_time, end_time, email_contact,amount_free_tickets,amount_payed_tickets,price_ticket) VALUES ('curso de desenho','desenhos realistas','2021-12-20','2021-12-22','18:00','21:30', 'luciane@hotmail.com',10,10,15.0 )
-INSERT INTO TB_EVENT (name, description, start_date, end_date, start_time, end_time, email_contact,amount_free_tickets,amount_payed_tickets,price_ticket) VALUES ('curso de violão','acordes básicos','2021-08-07','2021-08-08','19:30','22:00', 'pedro@gmail.com',10,10,5.0  )
-INSERT INTO TB_EVENT (name, description, start_date, end_date, start_time, end_time, email_contact,amount_free_tickets,amount_payed_tickets,price_ticket) VALUES ('curso de ukulele','notas e batidas','2021-05-07','2021-05-09','18:30','11:00', 'ricardo@hotmail.com',10,10,10.0  )
-
 INSERT INTO TB_PLACE (name, address) VALUES ('SorocabaClub','Rua São Bento n 121')
 INSERT INTO TB_PLACE (name, address) VALUES ('VotorantimClub','Avenida 31 de Março n 1000')
 INSERT INTO TB_PLACE (name, address) VALUES ('SoroTimClub','Avenida 01 de Abril n 42')
@@ -22,6 +17,11 @@ INSERT INTO TB_ATTEND(baseuser_id, balance) VALUES (4, 0.0)
 INSERT INTO TB_ATTEND(baseuser_id, balance) VALUES (5, 0.0)
 INSERT INTO TB_ATTEND(baseuser_id, balance) VALUES (6, 0.0)
 
-INSERT INTO TB_TICKET (price,type,date) VALUES (15.00,1,'2021-06-14')
-INSERT INTO TB_TICKET (price,type,date) VALUES (20.00,1,'2021-07-14')
-INSERT INTO TB_TICKET (price,type,date) VALUES (10.00,1,'2021-07-15')
+INSERT INTO TB_EVENT (name, description, start_date, end_date, start_time, end_time, email_contact,amount_free_tickets,amount_payed_tickets,price_ticket,admin_id,free_tickects_selled, payed_tickects_selled) VALUES ('curso de culinária','culinária francesa','2021-10-03','2021-10-05','19:30','10:00', 'gabi@gmail.com',10,10,10.0,1,0,0 )
+INSERT INTO TB_EVENT (name, description, start_date, end_date, start_time, end_time, email_contact,amount_free_tickets,amount_payed_tickets,price_ticket,admin_id,free_tickects_selled, payed_tickects_selled) VALUES ('curso de desenho','desenhos realistas','2021-12-20','2021-12-22','18:00','21:30', 'luciane@hotmail.com',10,10,15.0,2,0,0 )
+INSERT INTO TB_EVENT (name, description, start_date, end_date, start_time, end_time, email_contact,amount_free_tickets,amount_payed_tickets,price_ticket,admin_id,free_tickects_selled, payed_tickects_selled) VALUES ('curso de violão','acordes básicos','2021-08-07','2021-08-08','19:30','22:00', 'pedro@gmail.com',10,10,5.0,3,0,0  )
+INSERT INTO TB_EVENT (name, description, start_date, end_date, start_time, end_time, email_contact,amount_free_tickets,amount_payed_tickets,price_ticket,admin_id,free_tickects_selled, payed_tickects_selled) VALUES ('curso de ukulele','notas e batidas','2021-05-07','2021-05-09','18:30','11:00', 'ricardo@hotmail.com',10,10,10.0,3,0,0  )
+
+INSERT INTO TB_TICKET (type,date,event_id,attend_id) VALUES (1,'2021-06-14',1,4)
+INSERT INTO TB_TICKET (type,date,event_id,attend_id) VALUES (1,'2021-07-14',2,5)
+INSERT INTO TB_TICKET (type,date,event_id,attend_id) VALUES (1,'2021-07-15',3,6)
